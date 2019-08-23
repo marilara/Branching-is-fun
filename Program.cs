395925ecbd5a63e1d1c8b25f13fbe41fd4349f1a
maricela <laram@mail.gvsu.edu>
2019-08-22 20:43:34 -0400

    missing branching statement

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's do some branching!");
             Console.WriteLine("Hakuna Matata");
         }
     }
